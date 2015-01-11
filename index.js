6300a75e4ef3b00ef835ce14cf474d0067e10e26
Jordan Harband <ljharb@gmail.com>
2015-01-10 18:28:19 -0800

    Use `es-abstract` for ECMAScript spec internal abstract operations.

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
 'use strict';
 
 var define = require('define-properties');
+var ES = require('es-abstract/es6');
 var $isNaN = Number.isNaN || function (a) { return a !== a; };
 var $isFinite = Number.isFinite || function (n) { return typeof n === 'number' && global.isFinite(n); };
-var maxSafeInteger = Number.MAX_SAFE_INTEGER || (Math.pow(2, 53) - 1);
-var Obj = Object;
-var toStr = Obj.prototype.toString;
-var ES = {
-	CheckObjectCoercible: function (x, optMessage) {
-		if (x == null) {
-			throw new TypeError(optMessage || 'Cannot call method on ' + x);
-		}
-		return x;
-	},
-	ToObject: function (o, optMessage) {
-		return Obj(ES.CheckObjectCoercible(o, optMessage));
-	},
-	ToLength: function (value) {
-		var len = ES.ToInteger(value);
-		if (len <= 0) { return 0; } // includes converting -0 to +0
-		if (len > maxSafeInteger) { return maxSafeInteger; }
-		return len;
-	},
-	ToNumber: function (value) {
-		if (toStr.call(value) === '[object Symbol]') {
-			throw new TypeError('Symbols can not be converted to numbers');
-		}
-		return +value;
-	},
-	ToInteger: function (value) {
-		var number = ES.ToNumber(value);
-		if ($isNaN(number)) { return 0; }
-		if (number === 0 || !$isFinite(number)) { return number; }
-		return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
-	},
-	SameValueZero: function (a, b) {
-		return a === b || ($isNaN(a) && $isNaN(b));
-	}
-};
 
 var includesShim = function includes(searchElement) {
 	var fromIndex = arguments.length > 1 ? ES.ToInteger(arguments[1]) : 0;
@@ -63,7 +29,8 @@ var includesShim = function includes(searchElement) {
 /*eslint-disable no-unused-vars */
 var boundIncludesShim = function includes(array, searchElement) {
 /*eslint-enable no-unused-vars */
-	return includesShim.apply(ES.CheckObjectCoercible(array), Array.prototype.slice.call(arguments, 1));
+	ES.RequireObjectCoercible(array);
+	return includesShim.apply(array, Array.prototype.slice.call(arguments, 1));
 };
 define(boundIncludesShim, {
 	method: includesShim,
