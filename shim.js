4a0724bb6cac00ce449ebba73c1a37691f3d8c18
Jordan Harband <ljharb@gmail.com>
2015-08-15 23:15:19 -0700

    Assign the polyfill, not the implementation.

--- shim.js
+++ shim.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var define = require('define-properties');
-var implementation = require('./implementation');
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimArrayPrototypeIncludes() {
 	var polyfill = getPolyfill();
 	if (Array.prototype.includes !== polyfill) {
-		define(Array.prototype, { includes: implementation });
+		define(Array.prototype, { includes: polyfill });
 	}
 	return polyfill;
 };
