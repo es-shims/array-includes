dbf88e0c15f23f10bd4abe6a666910e0a3cc53b6
Jordan Harband <ljharb@gmail.com>
2016-11-22 11:54:07 -0600

    [Fix] ensure that `shim.js` actually shims when the polyfill differs from native.
    Note: At the current time, there are no implementations where this is true.

--- shim.js
+++ shim.js
@@ -5,8 +5,10 @@ var getPolyfill = require('./polyfill');
 
 module.exports = function shimArrayPrototypeIncludes() {
 	var polyfill = getPolyfill();
-	if (Array.prototype.includes !== polyfill) {
-		define(Array.prototype, { includes: polyfill });
-	}
+	define(
+		Array.prototype,
+		{ includes: polyfill },
+		{ includes: function () { return Array.prototype.includes !== polyfill; } }
+	);
 	return polyfill;
 };
